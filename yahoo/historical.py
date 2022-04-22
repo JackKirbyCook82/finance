85d0936827ebd908e2bf98ab8f389b7cee608bc5
JackKirbyCook82 <jackcookchee@yahoo.com>
2022-04-21 19:02:22 -0500

    Updated

--- yahoo/historical.py
+++ yahoo/historical.py
@@ -21,6 +21,7 @@ ROOT_DIR = os.path.abspath(os.path.join(MODULE_DIR, os.pardir))
 RESOURCE_DIR = os.path.join(ROOT_DIR, "resources")
 SAVE_DIR = os.path.join(ROOT_DIR, "save")
 REPOSITORY_DIR = os.path.join(SAVE_DIR, "yahoo")
+REPORT_FILE = os.path.join(REPOSITORY_DIR, "historical.csv")
 NORDVPN_EXE = os.path.join("C:/", "Program Files", "NordVPN", "NordVPN.exe")
 if ROOT_DIR not in sys.path:
     sys.path.append(ROOT_DIR)
@@ -30,6 +31,7 @@ from webscraping.webtimers import WebDelayer
 from webscraping.webloaders import WebLoader
 from webscraping.webreaders import WebReader, Retrys
 from webscraping.webquerys import WebQuery, WebDataset
+from webscraping.webqueues import WebScheduler, WebQueueable, WebQueue
 from webscraping.weburl import WebURL
 from webscraping.webdata import WebTable
 from webscraping.webpages import WebRequestPage, DataframeMixin
@@ -48,12 +50,11 @@ warnings.filterwarnings("ignore")
 
 historical_xpath = r"//table[@data-test='historical-prices']"
 historical_webloader = WebLoader(xpath=historical_xpath)
-datetime_parser = lambda x, **kw: {int: Datetime.fromtimestamp(x), str: Datetime.strptime(x, kw.get("format", "%d/%m/%Y"))}[type(x)](x) if not isinstance(x, Datetime) else x
+datetime_parser = lambda x, **kw: {tuple: Datetime(*x), int: Datetime.fromtimestamp(x), str: Datetime.strptime(x, kw.get("format", "%d/%m/%Y"))}[type(x)](x) if not isinstance(x, Datetime) else x
 filter_parser = lambda x, j: [i for i in x if i is not j]
-ticker_parser = lambda x: str(x).upper()
 date_parser = lambda x: Datetime.strptime(x, "%b %d, %Y")
-price_parser = lambda x: float(x)
 volume_parser = lambda x: int(str(x).replace(",", ""))
+ticker_parser = lambda x: str(x).upper()
 
 
 def historical_parser(dataframe, *args, ticker, **kwargs):
@@ -62,15 +63,29 @@ def historical_parser(dataframe, *args, ticker, **kwargs):
     dataframe.columns = [column.lower() for column in dataframe.columns]
     dataframe["ticker"] = ticker
     for column in dataframe.columns:
-        dataframe[column] = dataframe[column].apply(columns.get(column, price_parser))
+        dataframe[column] = dataframe[column].apply(columns.get(column, float))
     return dataframe
 
 
 class Yahoo_Finance_Historical(WebTable, loader=historical_webloader, parsers={"table": historical_parser}, optional=False): pass
 class Yahoo_Finance_WebDelayer(WebDelayer): pass
 class Yahoo_Finance_WebReader(WebReader, retrys=Retrys(retries=3, backoff=0.3, httpcodes=(500, 502, 504)), authenticate=None): pass
-class Yahoo_Finance_WebQuery(WebQuery, fields=["ticker"]): pass
+class Yahoo_Finance_WebQueue(WebQueue): pass
+class Yahoo_Finance_WebQuery(WebQuery, WebQueueable, fields=["ticker", "date"]): pass
 class Yahoo_Finance_WebDatasets(WebDataset[pd.DataFrame], ABC, fields=["historical"]): pass
+
+
+class Yahoo_Finance_WebScheduler(WebScheduler, fields=["ticker", "date"]):
+    @staticmethod
+    def ticker(*args, ticker=None, tickers=[], **kwargs): return [ticker_parser(x) for x in filter_parser([ticker, *tickers], None)]
+    @staticmethod
+    def date(*args, year=None, years=[], **kwargs): return [int(x) for x in filter_parser([year, *years], None)]
+
+    @staticmethod
+    def execute(querys, *args, **kwargs):
+        queueables = [Yahoo_Finance_WebQuery(query, name="YahooQuery") for query in querys]
+        queue = Yahoo_Finance_WebQueue(queueables, *args, name="YahooQueue", **kwargs)
+        return queue
 
 
 class Yahoo_Finance_WebURL(WebURL, protocol="https", domain="www.finance.yahoo.com"):
@@ -78,37 +93,38 @@ class Yahoo_Finance_WebURL(WebURL, protocol="https", domain="www.finance.yahoo.c
     def path(*args, ticker, **kwargs): return ["quote", ticker_parser(ticker), "history"]
 
     @staticmethod
-    def parm(*args, fromdate, todate, **kwargs):
+    def parm(*args, date, **kwargs):
         parm = {"interval": "1d", "filter": "history", "frequency": "1d", "includeAdjustedClose": "true"}
-        return {"period1": datetime_parser(fromdate, **kwargs), "period2": datetime_parser(todate, **kwargs), **parm}
+        return {"period1": datetime_parser((date, 1, 1), **kwargs), "period2": datetime_parser((date, 12, 31), **kwargs), **parm}
 
 
 class Yahoo_Finance_WebPage(DataframeMixin, WebRequestPage):
-    def execute(self, *args, ticker, **kwargs):
-        dataframe = Yahoo_Finance_Historical(self.source).data(*args, ticker, **kwargs)
-        return {"ticker": ticker}, "historical", dataframe
+    def execute(self, *args, ticker, date, **kwargs):
+        query = {"ticker": ticker, "date": date}
+        dataframe = Yahoo_Finance_Historical(self.source).data(*args, ticker=ticker, **kwargs)
+        return query, "historical", dataframe
 
 
 class Yahoo_Finance_WebDownloader(WebDownloader):
-    def execute(self, *args, reader, delayer, **kwargs):
-        tickers = filter_parser([kwargs.pop("ticker", None), *kwargs.pop("tickers", [])], None)
+    def execute(self, *args, scheduler, reader, delayer, **kwargs):
         with reader() as session:
             page = Yahoo_Finance_WebPage(session, name="YahooPage", delayer=delayer)
-            for ticker in tickers:
-                url = Yahoo_Finance_WebURL(*args, ticker=ticker, **kwargs)
-                page.load(str(url), referer=None)
-                page.setup(*args, **kwargs)
-                query, dataset, data = page(*args, ticker=ticker, **kwargs)
-                query = Yahoo_Finance_WebQuery(query, name="YahooQuery")
-                dataset = Yahoo_Finance_WebDatasets({dataset: data}, name="YahooDataset")
-                yield query, dataset
+            with scheduler(*args, **kwargs) as queue:
+                with queue:
+                    for query in queue:
+                        url = Yahoo_Finance_WebURL(**query.todict())
+                        page.load(str(url), referer=None)
+                        page.setup()
+                        fields, dataset, data = page(**query.todict())
+                        yield query, Yahoo_Finance_WebDatasets({dataset: data}, name="YahooDataset")
 
 
 def main(*args, **kwargs):
     delayer = Yahoo_Finance_WebDelayer(name="YahooDelayer", method="constant", wait=10)
     reader = Yahoo_Finance_WebReader(name="YahooReader")
+    scheduler = Yahoo_Finance_WebScheduler(name="USCensusScheduler", randomize=False, size=None, file=REPORT_FILE)
     downloader = Yahoo_Finance_WebDownloader(name="YahooDownloader", timeout=30)
-    downloader(*args, reader=reader, delayer=delayer, **kwargs)
+    downloader(*args, scheduler=scheduler, reader=reader, delayer=delayer, **kwargs)
     downloader.start()
     downloader.join()
     for query, results in downloader.results.items():
@@ -119,9 +135,9 @@ def main(*args, **kwargs):
 
 
 if __name__ == "__main__":
-    sys.argv += ["fromdate=01/01/2021", "todate=31/12/2021", "ticker=", "tickers=", "format=%d/%m/%Y"]
+    sys.argv += ["ticker=", "tickers=", "date=", "dates="]
     logging.basicConfig(level="INFO", format="[%(levelname)s, %(threadName)s]:  %(message)s", handlers=[logging.StreamHandler(sys.stdout)])
-    parsers = {"from": date_parser, "to": date_parser, "ticker": ticker_parser, "tickers": [ticker_parser(i) for i in lambda x: x.split(",")]}
+    parsers = {"ticker": ticker_parser, "tickers": lambda x: [ticker_parser(i) for i in x.split(",")], "year": int, "years": lambda x: [int(i) for i in x.split(",")]}
     inputparser = InputParser(proxys={"assign": "=", "space": "_"}, parsers=parsers, default=str)
     inputparser(*sys.argv[1:])
     main(*inputparser.arguments, **inputparser.parameters)
