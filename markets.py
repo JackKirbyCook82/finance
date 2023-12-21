f69023798857a5961bd499831246d9226bc71013
JackKirbyCook82 <jackcookchee@yahoo.com>
2023-12-20 21:02:15 -0600

    Updated 8/7/2023

--- markets.py
+++ markets.py
@@ -9,6 +9,7 @@ Created on Weds Jul 19 2023
 import logging
 import numpy as np
 import pandas as pd
+from collections import OrderedDict as ODict
 from collections import namedtuple as ntuple
 
 from support.pipelines import Processor
@@ -33,9 +34,9 @@ class MarketQuery(ntuple("Query", "current ticker expire valuation market")):
         market = cumulative.where(cumulative > funds)
         return MarketQuery(self.current, self.ticker, self.expire, self.valuation, market)
 
-    def curve(self, stop, *args, start=0, steps=10, **kwargs):
-        curve = pd.concat([self(funds).results for funds in reversed(range(start, stop, steps))], axis=0)
-        return CurveQuery(self.current, self.ticker, self.expire, self.valuation, curve)
+#    def curve(self, stop, *args, start=0, steps=10, **kwargs):
+#        curve = pd.concat([self(funds).results for funds in reversed(range(start, stop, steps))], axis=0)
+#        return CurveQuery(self.current, self.ticker, self.expire, self.valuation, curve)
 
     @property
     def results(self): return {"apy": self.apy, "npv": self.npv, "cost": self.cost, "size": self.size, "tau-": self.market["tau"].min(), "tau+": self.market["tau"].max()}
@@ -51,8 +52,8 @@ class MarketQuery(ntuple("Query", "current ticker expire valuation market")):
     def npv(self): return self.market["npv"] @ self.market["size"]
 
 
-class CurveQuery(ntuple("Query", "current ticker expire valuation curve")):
-    pass
+# class CurveQuery(ntuple("Query", "current ticker expire valuation curve")):
+#     pass
 
 
 class MarketCalculator(Processor):
@@ -63,18 +64,33 @@ class MarketCalculator(Processor):
         securities = {security: self.parser(security, dataframe, *args, **kwargs) for security, dataframe in securities.items()}
         securities = pd.concat(list(securities.values()), axis=1)
         for valuation, dataframe in valuations.items():
-
-            print(valuation)
-            print(dataframe)
-            print(securities)
-            raise Exception()
-
-            demand = dataframe.sort_values(["apy", "npv"], axis=0, ascending=False, inplace=False, ignore_index=True)
-            market = list(self.market(supply, demand))
+            dataframe = dataframe.sort_values(["apy", "npv"], axis=0, ascending=False, inplace=False, ignore_index=False)
+            market = list(self.market(securities, dataframe))
+            if not bool(market):
+                continue
             market = pd.DataFrame.from_records(market)
-            market = dataframe.where(market["size"] > 0)
+            market = market.where(market["size"] > 0)
             market = market.dropna(axis=0, how="all")
+            if bool(market.empty):
+                continue
             yield MarketQuery(query.current, query.ticker, query.expire, valuation, market)
+
+    @staticmethod
+    def market(supply, demand):
+        purchased = pd.DataFrame(0, index=supply.index, columns=supply.columns, dtype=np.int16)
+        for row in demand.itertuples(index=True):
+            available = supply - purchased
+            position = list(zip(demand.index.names, row.Index))
+            options = list(map(str, Securities.Options))
+            index = ODict([(key, value) for key, value in iter(position) if key not in list(options)])
+            strikes = ODict([(key, value) for key, value in iter(position) if key in list(options)])
+            columns = ODict([(column, getattr(row, column)) for column in demand.columns])
+            locators = [(tuple([*index.values(), strike]), option) for option, strike in strikes.items() if not np.isnan(strike)]
+            sizes = [available.loc[indx, cols] for (indx, cols) in locators]
+            size = np.min([row.size, *sizes])
+            for (indx, cols) in locators:
+                purchased.loc[indx, cols] = purchased.loc[indx, cols] + size
+            yield index | strikes | columns | {"size": size}
 
     @argsdispatcher(index=0)
     def parser(self, dataframe, *args, key, **kwargs): raise ValueError(str(key))
@@ -93,19 +109,6 @@ class MarketCalculator(Processor):
         dataframe = dataframe.drop_duplicates(subset=subset, keep="last", inplace=False)
         dataframe = dataframe.set_index(index, inplace=False, drop=True)
         return dataframe[columns]
-
-    @staticmethod
-    def market(supply, demand):
-        options = [str(option) for option in list(Securities.Options)]
-        for row in demand.itertuples():
-            row = row.to_dict()
-            strikes = [row[option] for option in options]
-            sizes = [supply.loc[strike, option] for strike, option in zip(strikes, options)]
-            size = np.min([row.pop("size")] + sizes)
-            for strike, option in zip(strikes, options):
-                supply.at[strike, option] = supply.loc[strike, option] - size
-            row = row | {"size": size}
-            yield row
 
 
 
