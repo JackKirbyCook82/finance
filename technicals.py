fd2ddedaadb82252a8638a00be29ac963c5be078
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-12-22 01:19:05 -0600

    Updated 1/10/2024

--- technicals.py
+++ technicals.py
@@ -8,10 +8,8 @@ Created on Fri Apr 19 2024
 
 import numpy as np
 import pandas as pd
-from enum import Enum
 from abc import ABC, ABCMeta
 from datetime import date as Date
-from types import SimpleNamespace
 
 from finance.concepts import Querys
 from calculations import Equation, Variables, Algorithms, Computations
@@ -223,100 +221,4 @@ class TechnicalCalculator(Sizing, Emptying, Partition, Logging, title="Calculate
 
     @property
     def equations(self): return self.__equations
-
-
-class TrendLineCalculator(Sizing, Emptying, Partition, Logging, title="Calculated"):
-    def __init__(self, *args, period, threshold, window, **kwargs):
-        assert isinstance(period, int) and period > 0 and period % 2 != 0
-        super().__init__(*args, **kwargs)
-        self.__threshold = threshold
-        self.__period = period
-        self.__window = window
-
-    def execute(self, technicals, /, **kwargs):
-        assert isinstance(technicals, pd.DataFrame)
-        if self.empty(technicals): return
-
-        close = technicals["close"]
-        threshold = technicals["ATR"]
-        pivots = self.pivots(close, period=self.period)
-        pivots = self.filters(pivots, threshold=threshold)
-        trendlines = self.trendlines(pivots, window=self.window)
-
-    @staticmethod
-    def pivots(series, /, period):
-        assert isinstance(series, pd.Series)
-        high = series.where(series.eq(series.rolling(period, center=True).max())).shift(period // 2)
-        low = series.where(series.eq(series.rolling(period, center=True).min())).shift(period // 2)
-        high = high.rename(f"{series.name}H")
-        low = low.rename(f"{series.name}L")
-        pivoted = SimpleNamespace(high=high, low=low)
-        return pivoted
-
-    @staticmethod
-    def filters(series, /, threshold):
-        assert isinstance(series, SimpleNamespace) and all([isinstance(value, pd.Series) for value in vars(series).values()])
-        assert len(series.high) == len(series.low)
-        size = min(len(series.high), len(series.low))
-        Pivots = Enum("Pivot", [("HIGH", 1), ("LOW", 2)])
-        array = np.full(size, np.NaN, dtype=float)
-        filtered = SimpleNamespace(high=array.copy(), low=array)
-        unfiltered = SimpleNamespace(high=series.high, low=series.low)
-        last = SimpleNamespace(high=np.NaN, low=np.NaN)
-        pivot = np.NaN
-        for index in range(size):
-            criteria = threshold.iloc[index] if isinstance(threshold, pd.Series) else float(threshold)
-            if not np.isnan(unfiltered.high.iloc[index]) and (np.isnan(last.low) or abs(unfiltered.high.iloc[index] - last.low) >= criteria):
-                if pivot != Pivots.HIGH or np.isnan(last.high) or unfiltered.high.iloc[index] > last.high:
-                    filtered.high[index] = last.high = unfiltered.high.iloc[index]
-                    pivot = Pivots.HIGH
-            if not np.isnan(unfiltered.low.iloc[index]) and (np.isnan(last.high) or abs(last.high - unfiltered.low.iloc[index]) >= criteria):
-                if pivot != Pivots.LOW or np.isnan(last.low) or unfiltered.low.iloc[index] < last.low:
-                    filtered.low[index] = last.low = unfiltered.low.iloc[index]
-                    pivot = Pivots.LOW
-        high = pd.Series(filtered.high, index=series.high.index, name=series.high.name)
-        low = pd.Series(filtered.low, index=series.low.index, name=series.low.name)
-        filtered = SimpleNamespace(high=high, low=low)
-        return filtered
-
-    @staticmethod
-    def trendlines(series, /, window):
-        assert isinstance(series, SimpleNamespace) and all([isinstance(value, pd.Series) for value in vars(series).values()])
-        assert len(series.high) == len(series.low)
-        size = min(len(series.high), len(series.low))
-        indexes = np.arange(size)
-        array = np.full(size, np.NaN, dtype=float)
-        trendlines = SimpleNamespace(support=array.copy(), resistance=array)
-        points = SimpleNamespace(support=[], resistance=[])
-        for index in range(size):
-            value = SimpleNamespace(support=series.low.iloc[index], resistance=series.high.iloc[index])
-            if not np.isnan(value.support):
-                point = SimpleNamespace(x=index, y=float(value.support))
-                points.support.append(point)
-                if len(points.support) > window: points.support.pop(0)
-            if not np.isnan(value.resistance):
-                point = SimpleNamespace(x=index, y=float(value.resistance))
-                points.resistance.append(point)
-                if len(points.resistance) > window: points.resistance.pop(0)
-            if len(points.support) >= 2:
-                x = np.array([point.x for point in points.support], dtype=float)
-                y = np.array([point.y for point in points.support], dtype=float)
-                a, b = np.polyfit(x, y, 1)
-                trendlines.support[index] = a * indexes[index] + b
-            if len(points.resistance) >= 2:
-                x = np.array([point.x for point in points.resistance], dtype=float)
-                y = np.array([point.y for point in points.resistance], dtype=float)
-                a, b = np.polyfit(x, y, 1)
-                trendlines.resistance[index] = a * indexes[index] + b
-        support = pd.Series(trendlines.support).rename(f"{series.support.name}SPT")
-        resistance = pd.Series(trendlines.resistance).rename(f"{series.resistance.name}RST")
-        trendlines = SimpleNamespace(support=support, resistance=resistance)
-        return trendlines
-
-    @property
-    def threshold(self): return self.__threshold
-    @property
-    def period(self): return self.__period
-    @property
-    def window(self): return self.__window
 
